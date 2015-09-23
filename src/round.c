afd32a06d17db6b459ec340fe00bbf2f7c04e6cc
Cherie Williams <cherie@getpebble.com>
2015-09-22 21:56:16 -0700

    Update w/new radial/inset APIs

--- src/round.c
+++ src/round.c
@@ -8,28 +8,33 @@
 #define HOUR_ANGLE(hour) hour * (TRIG_MAX_ANGLE / 12)
 
 // Draw an arc with given inner/outer radii 
-static void draw_arc(GContext *ctx, Layer *layer, uint16_t inner_radius, uint16_t outer_radius, uint32_t end_angle) {
+static void draw_arc(GContext *ctx, GRect rect, uint16_t thickness, uint32_t end_angle) {
   if(end_angle == 0) {
-    graphics_fill_radial(ctx, layer_get_center(layer), inner_radius, outer_radius, 0, TRIG_MAX_ANGLE);
+    graphics_fill_radial(ctx, rect, GOvalScaleModeFitCircle, thickness, 0, TRIG_MAX_ANGLE);
   }
   else {
-    graphics_fill_radial(ctx, layer_get_center(layer), inner_radius, outer_radius, 0, end_angle); 
+    graphics_fill_radial(ctx, rect, GOvalScaleModeFitCircle, thickness, 0, end_angle); 
   }
+}
+
+static GRect calculate_rect(Layer *layer, uint8_t arc_id) {
+  uint8_t padding = 10;
+  return grect_inset(layer_get_bounds(layer), GEdgeInsets(padding*(3*arc_id)));
 }
 
 // Handle representation for seconds
 void draw_seconds(GContext *ctx, uint8_t seconds, Layer *layer) {
-  draw_arc(ctx, layer, 70, 90, TIME_ANGLE(seconds));
+  draw_arc(ctx, calculate_rect(layer, 0), 20, TIME_ANGLE(seconds));
 }
 
 // Handle representation for minutes
 void draw_minutes(GContext *ctx, uint8_t minutes, Layer *layer) {
-  draw_arc(ctx, layer, 40, 60, TIME_ANGLE(minutes));
+  draw_arc(ctx, calculate_rect(layer, 1), 20, TIME_ANGLE(minutes));
 }
 
 // Handle representation for hours
 void draw_hours(GContext *ctx, uint8_t hours, Layer *layer) {
-  draw_arc(ctx, layer, 10, 30, HOUR_ANGLE(hours));
+  draw_arc(ctx, calculate_rect(layer, 2), 20, HOUR_ANGLE(hours));
 }
 
 #endif
